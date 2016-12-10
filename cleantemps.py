be40892394c35efa88476f939f8d2670f7b69184
Brett Wilson <drytoastman@gmail.com>
2016-12-09 23:35:31 -0800

    add script to scrub state database of weird things that my thermostat reports once a day or so.  Set temperature at 28F for 60 seconds? no, I don't think so

---
+++ cleantemps.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+import json
+import sqlite3
+try:
+    # Find errneous temp states reported by the thermostat
+    # If SET is outside (50,85) or MEASURED is outside (45,100) delete
+    conn = sqlite3.connect("home-assistant_v2.db")
+    todel = list()
+    for row in conn.execute("select state_id,attributes from states where domain='climate'"):
+        attr = json.loads(row[1])
+        if attr.get('temperature', 70) not in range(50, 85) or attr.get('current_temperature', 70) not in range(45, 100):
+            todel.append(str(row[0]))
+    count = conn.execute("delete from states where state_id in (:todel)", {"todel":','.join(todel)}).rowcount
+    print("Deleted {} rows".format(count))
+    conn.commit()
+    conn.close()
+except Exception as e:
+    print("Failed to clean: {}".format(e))
+
